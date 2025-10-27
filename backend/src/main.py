abf8c83bdfdbf4800524dddf0b116bb5f14dca98
lcstrangman <lcstrangman@wpi.edu>
2025-10-27 15:28:36 -0400

    Updated frontend framework to use vite
    Ran basic test, should be able to connect to the camera stream url
    
    Run docker-compose up to use
    
    May want to run npm install in the frontend folder if you don't want it to show errors

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+#app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Allow the frontend (served on localhost:3000) to call backend APIs during development
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/media/{stream_name}")
 def read_item(stream_name: str):
