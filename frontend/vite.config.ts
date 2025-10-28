6d10c818eef67e6a1496c58e9dc72d2507918ffe
Leo Strangman <lcstrangman@wpi.edu>
2025-10-27 21:05:16 -0400

    Updated docker compose file and configurations
    Fixed docker compose and frontend dockerfile so the files should properly live-update without needing to restart the docker container.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  plugins: [],
+  
+  // Add this 'server' block
   server: {
-    host: true,
+    host: '0.0.0.0', // Allows access from your host machine
     port: 3000,
-  },
+    watch: {
+      usePolling: true // Enables polling for file changes
+    }
+  }
 })
