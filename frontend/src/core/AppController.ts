74acafc37fe1b32531833a6a1bb4233214a3a889
KimAllise32 <kacummings@wpi.edu>
2026-02-16 13:56:33 -0500

    fixed the offset for the green cursor

--- frontend/src/core/AppController.ts
+++ frontend/src/core/AppController.ts
@@ -487,11 +487,6 @@ class AppController {
                 zoomWrapper.style.transform = `scale(${zoomLevel})`;
             }
 
-            if (zoomLevel > 1) {
-                this.ui.viewport.style.overflow = 'auto';
-            } else {
-                this.ui.viewport.style.overflow = 'hidden';
-            }
 
 
         }, { passive: false });
@@ -530,12 +525,6 @@ class AppController {
 
                 zoomWrapper.style.transform = `scale(${zoomLevel})`;
 
-                // show scrollbars when zoomed in, hide at default
-                if (zoomLevel > 1) {
-                    this.ui.viewport.style.overflow = 'auto';
-                } else {
-                    this.ui.viewport.style.overflow = 'hidden';
-                }
             }
         }, { passive: false });
 
